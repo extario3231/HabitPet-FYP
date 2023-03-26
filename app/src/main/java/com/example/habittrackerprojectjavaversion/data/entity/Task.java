5b02383fd40e7787e7e24a0e9ecd882748677f58
extario3231 <ycp0021@gmail.com>
2023-03-26 14:04:39 +0800

    add one column

--- app/src/main/java/com/example/habittrackerprojectjavaversion/data/entity/Task.java
+++ app/src/main/java/com/example/habittrackerprojectjavaversion/data/entity/Task.java
@@ -6,27 +6,29 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "task", foreignKeys = {@ForeignKey(entity = Habit.class, parentColumns = {"id"},
-        childColumns = {"habit_id"}, onDelete = ForeignKey.CASCADE)})
+@Entity(tableName = "task")
 public class Task {
     @PrimaryKey(autoGenerate = true)
-    private long id;
+    @ColumnInfo(name = "task_id")
+    private long taskId;
     @NonNull
     private String name;
-    @ColumnInfo(name = "habit_id")
+    private int period;
+    @ColumnInfo(name = "habit_task_id")
     private long habitId;
 
-    public Task(String name, long habitId) {
+    public Task(@NonNull String name, int period, long habitId) {
         this.name = name;
+        this.period = period;
         this.habitId = habitId;
     }
 
-    public long getId() {
-        return id;
+    public long getTaskId() {
+        return taskId;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setTaskId(long taskId) {
+        this.taskId = taskId;
     }
 
     public String getName() {
@@ -35,6 +37,14 @@ public class Task {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
     }
 
     public long getHabitId() {
