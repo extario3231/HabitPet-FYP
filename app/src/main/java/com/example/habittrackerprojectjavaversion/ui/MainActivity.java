ed8b8a7e9892dc65997874e20672ac1b4fbb55c6
extario3231 <ycp0021@gmail.com>
2023-04-05 18:09:57 +0800

    initialize the database when the app starts

--- app/src/main/java/com/example/habittrackerprojectjavaversion/ui/MainActivity.java
+++ app/src/main/java/com/example/habittrackerprojectjavaversion/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.habittrackerprojectjavaversion.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -11,10 +12,18 @@ import android.widget.ImageView;
 
 import com.example.habittrackerprojectjavaversion.R;
 import com.example.habittrackerprojectjavaversion.data.AppDatabase;
+import com.example.habittrackerprojectjavaversion.data.entity.PetProgress;
+import com.example.habittrackerprojectjavaversion.data.repo.PetProgressRepo;
+
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 
 public class MainActivity extends Activity {
     private ImageView myImageView;
     private static AppDatabase db;
+    private PetProgressRepo progressRepo;
+    private final String TAG = "MainActivity";
+    private CompositeDisposable compositeDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +39,11 @@ public class MainActivity extends Activity {
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.show_and_hide);
         myImageView.startAnimation(animation);
 
+        compositeDisposable = new CompositeDisposable();
         db = AppDatabase.getDatabase(getApplicationContext());
+        progressRepo = new PetProgressRepo(db);
+        Disposable disposable = initDb();
+        compositeDisposable.add(disposable);
     }
 
     private void initViews() {
@@ -56,7 +69,22 @@ public class MainActivity extends Activity {
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+
     public static AppDatabase getDb() {
         return db;
+    }
+
+    public Disposable initDb() {
+        PetProgress progress = db.petProgressDao().findProgress();
+        if (progress != null) return Disposable.empty();
+
+        return progressRepo.insert(new PetProgress("/res/drawable/cat.png", 0))
+                .subscribe(() -> Log.i(TAG, "init complete."),
+                        err -> Log.e(TAG, "init failed: " + err.getMessage()));
     }
 }
