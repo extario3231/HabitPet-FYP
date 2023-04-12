6edb3c97a455699be28066dca56ba89454a75687
toolongdontread <kcltommy01@gmail.com>
2023-04-12 18:16:48 +0800

    add CalendarProgress to db

--- app/src/main/java/com/example/habitpet/data/AppDatabase.java
+++ app/src/main/java/com/example/habitpet/data/AppDatabase.java
@@ -6,17 +6,20 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.habitpet.data.dao.CalendarProgressDao;
 import com.example.habitpet.data.dao.HabitDao;
 import com.example.habitpet.data.dao.PetProgressDao;
+import com.example.habitpet.data.entity.CalendarProgress;
 import com.example.habitpet.data.entity.Habit;
 import com.example.habitpet.data.entity.PetProgress;
 
-@Database(entities = {Habit.class, PetProgress.class}, version = 1)
+@Database(entities = { Habit.class, PetProgress.class, CalendarProgress.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase instance;
 
     public abstract HabitDao habitDao();
     public abstract PetProgressDao petProgressDao();
+    public abstract CalendarProgressDao calendarProgressDao();
 
     public static AppDatabase getDatabase(Context context) {
         if (instance == null)
